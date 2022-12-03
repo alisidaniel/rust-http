346ddad994f7fb3536446768856783c4f69205a2
Daniel Alisi <alisidaniel@gmail.com>
2022-12-03 13:16:05 +0100

    feat: status code

--- target/debug/deps/server-cf16183feebbf1d0.d
+++ target/debug/deps/server-cf16183feebbf1d0.d
@@ -1,10 +1,12 @@
-/home/daniel/Public/dev/rust-http/target/debug/deps/server-cf16183feebbf1d0.rmeta: src/main.rs src/http/mod.rs src/http/method.rs src/http/query_string.rs src/http/request.rs src/server.rs
+/home/daniel/Public/dev/rust-http/target/debug/deps/server-cf16183feebbf1d0.rmeta: src/main.rs src/http/mod.rs src/http/method.rs src/http/query_string.rs src/http/request.rs src/http/response.rs src/http/status_code.rs src/server.rs
 
-/home/daniel/Public/dev/rust-http/target/debug/deps/server-cf16183feebbf1d0.d: src/main.rs src/http/mod.rs src/http/method.rs src/http/query_string.rs src/http/request.rs src/server.rs
+/home/daniel/Public/dev/rust-http/target/debug/deps/server-cf16183feebbf1d0.d: src/main.rs src/http/mod.rs src/http/method.rs src/http/query_string.rs src/http/request.rs src/http/response.rs src/http/status_code.rs src/server.rs
 
 src/main.rs:
 src/http/mod.rs:
 src/http/method.rs:
 src/http/query_string.rs:
 src/http/request.rs:
+src/http/response.rs:
+src/http/status_code.rs:
 src/server.rs:
